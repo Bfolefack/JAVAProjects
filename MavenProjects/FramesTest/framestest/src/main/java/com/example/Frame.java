d146ecb29e54af9ce657c4475867f562eb9091c8
Boueny Folefack <bouenyfolefack@gmail.com>
2021-09-02 14:25:35 -0500

    Created FramesTest to practice with JFrame

---
+++ MavenProjects/FramesTest/framestest/src/main/java/com/example/Frame.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class Frame 
+{
+    public static void main( String[] args )
+    {
+        JFrame jFrame = new JFrame("The Longest Frame Title in the history of java and the world ever");
+        jFrame.setVisible(true);
+        System.out.println( "Hello World!" );
+    }
+}
