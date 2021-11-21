a32a776df71bba8c2405413f88a40253208b1aa2
Boueny Folefack <bouenyfolefack@gmail.com>
2021-11-21 17:12:54 -0600

    Moved all neural network related java projects to their own folder

---
+++ MavenProjects/NeuralNetworks/ToyNeuralNetwork/toyneuralnetwork/src/main/java/com/example/Data/XORData.java
@@ -0,0 +1,9 @@
+package com.example.Data;
+public class XORData{
+  public float[] inputs;
+  public float[] ans;  
+  public XORData(float i1, float i2, float a){
+    inputs = new float[]{i1, i2};
+    ans = new float[]{a};
+  }
+}
